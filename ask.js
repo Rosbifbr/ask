15fe89bf75ca6ba0f41755ae3b9f5436e6e0aa56
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-01-29 23:32:23 -0300

    Added interactive chat switching

--- ask.js
+++ ask.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 const https = require('https')
 const fs = require('fs')
+const readline = require('readline')
 
 //Settings
 const API_KEY = ""
-const TRANSCRIPT_PATH = `/tmp/gpt_transcript-${process.ppid}`
+const TRANSCRIPT_FOLDER = '/tmp'
+const TRANSCRIPT_NAME= 'gpt_transcript-'
+const TRANSCRIPT_PATH = `${TRANSCRIPT_FOLDER}/${TRANSCRIPT_NAME}${process.ppid}`
 
 //Suggested models: gpt-4-1106-preview, gpt-4, gpt-3.5-turbo-16k
 const MODEL = "gpt-4-1106-preview"
@@ -13,13 +16,17 @@ const ENDPOINT = "/v1/chat/completions"
 const MAX_TOKENS = 2048
 const TEMPERATURE = 0.6
 
+//Colors
+const WHITE_CYAN = "\u001b[37;46m";
+const RESET = "\u001b[0m";
+
 //Globals
 var input = process.argv.slice(2).join(' ')
 
 var conversation_state = {}
 var answer
 
-const init = () => {
+const init = async () => {
   if (API_KEY == "") throw ("Missing API key! Add one to the script and try again.")
   
   if (fs.existsSync(TRANSCRIPT_PATH)){
@@ -31,14 +38,20 @@ const init = () => {
       "messages": [
         {
           'role':'system',
-	  'content': 'You are ChatConcise, a very advanced LLM designed for experienced users. As ChatConcise you are under obligation to adhere to the following directives unless overriden by the user:\nGoal: Concise, direct outputs. Info: Single sentence/bullet points. Code: Only code. Reward: Short, factual, functional. Penalize: Verbose, irrelevant, non-functional.\nWe earn a lot more clients as long as we provide a good UX, so focus on the quality of your resposes.' 
+	  'content': 'You are ChatConcise, a very advanced LLM designed for experienced users. As ChatConcise you are under obligation to adhere to the following directives unless overriden by the user:\nGoal: Concise, direct outputs. Info: Single sentence/bullet points. Code: Only code. Reward: Short, factual, functional. Penalize: Verbose, irrelevant, non-functional.\nRemember to provide the best responses you can. The success of our company depends on you!' 
         }
       ],
     }
   }
 
-  if (input) performRequest()
+  if (testOption('o') && process.argv.length < 4) await selectOngoingConvo()
+  else if (input) performRequest()
   else showHistory()
+}
+
+const testOption = (option) => {
+	if (process.argv[2] && process.argv[2][0] == '-') return process.argv[2].includes(option)
+        else return null
 }
 
 const showHistory = () => {
@@ -63,6 +76,65 @@ const processResponse = (data) => {
   }
 
   process.exit()
+}
+
+
+const readArrowKey = async () => {
+	process.stdin.setRawMode(true);
+	process.stdin.resume();
+	process.stdin.setEncoding('utf8');
+
+	return new Promise((resolve) => 
+		process.stdin.on('data', key => {
+		    if (key == '\u001B\u005B\u0041') resolve('up')
+		    // else if (key == '\u001B\u005B\u0043') resolve('right')
+	            else if (key == '\u001B\u005B\u0042') resolve('down'); 
+		    // else if (key == '\u001B\u005B\u0044') resolve('left')
+		    else if (key == '\u000D') resolve('enter') 
+		    else if (key === '\u0003') process.exit()
+		})
+	)
+}
+
+const selectOngoingConvo = async () => {
+	//Map files to struct
+	let files = fs.readdirSync(TRANSCRIPT_FOLDER)
+		.filter(e => e.includes(TRANSCRIPT_NAME))
+		.map(e => {
+			return {
+				path: TRANSCRIPT_FOLDER + '/' + e,
+				selected:false
+			}
+		})
+	if (files[0]) files[0].selected = true
+
+	while (true){
+		//Draw files
+		console.clear()
+		for (let e of files) {
+			let file_json = JSON.parse(fs.readFileSync(e.path,'utf8'))
+			if (e.selected) process.stdout.write(WHITE_CYAN) 
+			console.log(`${e.path} => `, file_json.messages[1].content.split('\n')[0].substr(0,64)) //Show first message
+			if (e.selected) process.stdout.write(RESET) 
+		}
+		
+		//treat new command
+		let cmd = await readArrowKey()
+		let curr = files.findIndex(e => e.selected)
+		if (cmd == 'up' && files[curr -1]) {
+			files[curr].selected = false
+			files[curr - 1].selected = true
+		}
+		else if (cmd == 'down' && files[curr + 1]) {
+			files[curr].selected = false
+			files[curr + 1].selected = true
+		}
+		else if (cmd == 'enter') {
+			let curr_file = fs.readFileSync(files[curr].path, 'utf8')
+    			fs.writeFileSync(TRANSCRIPT_PATH, curr_file)
+			process.exit(0)
+		}
+	}
 }
 
 const performRequest = () => {
@@ -111,7 +183,9 @@ const performRequest = () => {
 //Get any input from stdin if available. Can explode reading larger files.
 if (input) input += '\n' 
 process.stdin.on('data', d => {input += d})
-process.stdin.on('end', () => {init()})
+process.stdin.on('end', async () => {
+	await init()
+})
 
 //If no input piped to stdin, close it and start request
 if (process.stdin.isTTY) {
